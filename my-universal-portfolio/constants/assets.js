0981b1f51098192f721220d4a7c1602c424889a0
mdgufranyt <gufranbareilly@gmail.com>
2025-07-19 22:27:26 +0530

    update

--- my-universal-portfolio/constants/assets.js
+++
@@ -1,7 +0,0 @@
-export const profileImage = require('../assets/images/gufraaan.jpeg');
-export const resumePdf = require('../assets/MDGufran_Resume.pdf');
-// Add other assets here as you build more components
-export const project1 = require('../assets/images/portfolio1.png');
-export const project2 = require('../assets/images/portfolio3.png');
-export const project3 = require('../assets/images/portfolio4.png');
-export const project4 = require('../assets/images/portfolio5.png');
